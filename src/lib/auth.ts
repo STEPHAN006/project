7b72bc1bf382f75e844b19a89a41a33c970adb78
Arovana <stephystephan13@gmail.com>
2025-04-11 13:49:02 +0300

    add better auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,25 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "sqlite", // or "mysql", "postgresql", ...etc
+    }),
+    emailAndPassword: {
+        enabled: true,
+        async sendResetPassword(data, request) {
+            // Send an email to the user with a link to reset their password
+        },
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        },
+    },
+});
